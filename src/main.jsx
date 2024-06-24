55c25565b84adbce9ef7649c11923187775f5241
shivamdev24 <shivam.dev2402@gmail.com>
2024-06-24 12:00:52 +0530

    test routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,37 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-import './index.css'
-
-import {
-  BrowserRouter,
-  Routes,
-  Route
-  
-} from "react-router-dom";
-import Home from './pages/Home.jsx';
-import Contact from './pages/Contact.jsx';
-import Service from './pages/Service.jsx';
-
-
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Contact from "./pages/Contact.jsx";
+import Service from "./pages/Service.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    
-
     <BrowserRouter>
-    <Routes>
-      <Route path='/'  element={<App />}  >
-      <Route path=''  element={<Home />}  />
-      <Route path='/contact'  element={<Contact />}  />
-      <Route path='/service'  element={<Service />}  />
-
-
-      </Route>
-    </Routes>
-    
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="service" element={<Service />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
